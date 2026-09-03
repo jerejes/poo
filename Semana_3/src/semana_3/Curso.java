f506abd52244d2a1d9557738be95e4de01c7d4d9
joaq <joaquinquispe2022@gmail.com>
2026-09-03 11:29:10 -0500

    xd

--- Semana_3/src/semana_3/Curso.java
+++ Semana_3/src/semana_3/Curso.java
@@ -37,12 +37,6 @@ public Object [] registrar (int num , String alumno )
 {
     Object [] fila = {num,nomCur,tipo,modali,ciclo, alumno,costo} ; 
     return fila;
-    string a ="da";
-        string a ="da";
-    string a ="da";
-    string a ="da";
-    string a ="da";
-
 }
 
 }
